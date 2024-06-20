26062506c485beb658adf3f7467b1f11aa6c1b6f
Musti <mustafaa.demirhann.0@gmail.com>
2024-06-21 00:03:51 +0300

    updated pwa

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,6 +27,11 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <meta name="theme-color" content="#000000" />
+      </head>
       <body className={jetbrainsMono.className}>
         <AuthContextProvider>
           <Header />

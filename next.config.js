2e68fdbc6a43df10fb7348008d5bfe943dc3512b
MustafaDemirhan <mustafaa.demirhann.0@gmail.com>
2025-03-10 04:26:02 +0300

    update build

--- next.config.js
+++ next.config.js
@@ -1,32 +1,32 @@
-const withPWA = require('next-pwa')({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-  });
-  
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    images: {
-      domains: ['your-storage-domain.com'],
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '**',
-        },
-      ],
-    },
-    pwa: {
-      dest: 'public',
-      register: true,
-      skipWaiting: true,
-      disable: process.env.NODE_ENV === 'development'
-    }
-  };
-  
-  const withPWA = require('next-pwa')(nextConfig);
-  
-  module.exports = withPWA;
+const withPWA = require('next-pwa');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['your-storage-domain.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  }
+};
+
+// PWA yapılandırmasını ayrı bir obje olarak tanımlayalım
+const pwaConfig = {
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+};
+
+// eslint yapılandırmasını PWA yapılandırmasından ayıralım
+module.exports = {
+  ...withPWA(pwaConfig),
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  ...nextConfig
+};
   
